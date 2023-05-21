0c33bacde0404e78a99d4c39d7cedabdc8240c40
sanddep-dev <sandeep.n062000@gmail.com>
2023-05-21 20:55:24 +0530

    projects skeleton

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import './Projects.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import { EffectFade, Pagination } from "swiper";
 
 const Projects = () => {
   return (
     <div className="projects">
       <div className="projects-container">
-        Projects
+        <div className="projects-header">
+          <span>Somme Things I've Built</span>
+          <h2>Projects</h2>
+        </div>
+        <div className="projects-content">
+          <Swiper
+          pagination={true}
+          modules={[EffectFade,Pagination]}
+          effect={"fade"}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+          }}
+          >
+            <SwiperSlide className='projects-slider'>
+              <div className="projects-slider_img">
+                <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp" alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className='projects-slider'>
+              <div className="projects-slider_img">
+                <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/jason-leung-798979-unsplash.webp" alt="" />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
   )
