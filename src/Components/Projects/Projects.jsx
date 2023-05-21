ad71e84e03dedb7ae02d273aab004dc48c71be83
sanddep-dev <sandeep.n062000@gmail.com>
2023-05-21 15:51:36 +0530

    updated skills section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,8 +5,13 @@ const Projects = () => {
   return (
     <div className="projects">
         <div className="projects-container">
+          <div className="projects-content">
             <span>Some Things I’ve Built</span>
             <h2>Projects</h2>
+          </div>
+          <div className="projects-content-wrapper">
+            project details
+          </div>
         </div>
     </div>
   )
